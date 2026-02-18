147ae4401a5fce00054804b9d0ff49b96d93ab70
Luke Schierer <luke@schierer.org>
2026-02-18 00:59:52 -0500

    build: a bit of cleanup

--- packages/luke/lib/App/RecentChanges.pm
+++ packages/luke/lib/App/RecentChanges.pm
@@ -10,7 +10,6 @@ our $VERSION = '0.00.1';
 class App::RecentChanges {
   use Exporter qw(import);
   require JSON::PP;
-  require YAML::PP;
 
   use Path::Tiny;
   use HTML::Entities qw(encode_entities);
@@ -50,7 +49,7 @@ class App::RecentChanges {
     99174ee8 70ef2b87 62869f07
     09737f68 fdd3023e 647de408
     302d1a51 6e677bcd fc3efc8f
-    6ed6fa6b
+    6ed6fa6b 679d474c
   );
 
   field $json =
@@ -62,163 +61,10 @@ class App::RecentChanges {
     ->convert_blessed()
     ->canonical();
 
-  field $ypp = YAML::PP->new(
-    schema       => [qw/ + Perl /],
-    yaml_version => ['1.2', '1.1'],
-  );
 
   field $RUT_dir = Path::Tiny::path("./log");
 
-  # Update the log/index.md file with recent changes
-  method update_recent_changes {
-    my ($json_file, $md_file, $limit) = @_;
-    $limit //= 100;    # Default to 100 entries
-
-    # Read the JSON file
-
-    my $json_content = path($json_file)->slurp_utf8;
-    my $data;
-    $data = $json->decode($json_content);
-
-    # Filter entries with non-empty files array and limit to specified number
-    my @filtered_entries;
-    foreach my $entry (@$data) {
-      next unless $entry->{files} && @{ $entry->{files} } > 0;
-      push @filtered_entries, $entry;
-      last if scalar(@filtered_entries) >= $limit;
-    }
-
-    # Generate the definition list HTML
-    my $dl_html = "<dl class=\"recent-changes\">\n";
-
-    foreach my $entry (@filtered_entries) {
-
-      my $short_id   = substr($entry->{id}, 0, 12);
-      my $commit_url = "https://github.com/lschierer/RUT/commit/$entry->{id}";
-      if ( (!exists $entry->{files})
-        or (ref($entry->{files}) ne 'ARRAY')
-        or (scalar @{ $entry->{files} } == 0)) {
-        next;
-      }
-
-      my %displayedPaths;
-      my $files_html = '';
-      foreach my $file (@{ $entry->{files} }) {
-
-        # Clean up the file name
-        $file =~ s/^\s+|\s+$//g;
-        $file =~ s/^"(.*)"$/$1/ if $file =~ /^".*"$/;
-        next unless $file;
-
-        # Extract just the filename without extension and path
-        # Handle both .md and .mdwn extensions
-        if ($file =~ m{(?:packages/)?(?:luke/)?(?:log/)?(.+?)\.(?:md|mdwn)$}) {
-          my $path         = $1;
-          my $display_name = $path;
-
-          # Get just the last part of the path for display name
-          if ($path =~ m{.*/([^/]+)$}) {
-            $display_name = $1;
-          }
-          if (!exists $displayedPaths{ encode_entities($path) }) {
-            $displayedPaths{ encode_entities($path) }++;
-
-            if (Path::Tiny::path("./log/$path.md")->is_file()) {
-              my $content   = Path::Tiny::path("./log/$path.md")->slurp_utf8();
-              my $yaml_data = {};
-              if ($content =~ s/^---\s*\n(.*?)\n---\s*\n//s) {
-                my $yaml = $1;
-                eval { $yaml_data = $ypp->load_string($yaml); };
-                if ($@) {
-                  say "Error parsing YAML front matter: $@";
-                }
-                elsif (ref $yaml_data eq 'HASH') {
-                  # Use title from front matter if available
-                  $display_name = $yaml_data->{title}
-                    if exists $yaml_data->{title};
-                }
-              }
-              # Create the link with the desired format
-              $files_html .=
-"    <li><a href=\"/~luke/log/$path/\">$display_name</a></li>\n";
-            }
-            elsif (Path::Tiny::path("./log/$path.mdwn")->is_file()) {
-              $files_html .=
-"    <li><a href=\"/~luke/log/$path/\">$display_name</a></li>\n";
-            }
-            else {
-              $displayedPaths{ encode_entities($path) }--;
-              if ($displayedPaths{ encode_entities($path) } <= 0) {
-                $displayedPaths{ encode_entities($path) } = undef;
-                delete $displayedPaths{ encode_entities($path) };
-              }
-            }
-
-          }
-          if (keys %displayedPaths > 10) {
-            my $remaining = scalar @{ $entry->{files} };
-            $remaining = $remaining - (keys %displayedPaths);
-            say "I have $remaining entries I am skipping for $short_id";
-            if ($remaining == 1) {
-              $files_html .= "    <li>and $remaining additional file.</li>\n";
-            }
-            elsif ($remaining > 1) {
-              $files_html .= "    <li>and $remaining additional files.</li>\n";
-            }
-            last;
-          }
-
-        }
-        else {
-          # Fallback for files that don't match the expected pattern
-          $files_html .= "    <li>" . encode_entities($file) . "</li>\n";
-        }
-      }
-
-      my $entry_html = '';
-      # Format the date as ISO 8601
-      my $timestamp = $entry->{date};
-      my $dt        = DateTime->from_epoch(epoch => $timestamp);
-      my $iso_date  = $dt->iso8601();
-
-      # Get the commit message (first line of the message array)
-      my $message = $entry->{message} // '';
-
-      # Add to the definition list
-      $entry_html .= "  <dt><a href=\"$commit_url\">$short_id</a></dt>\n";
-      $entry_html .= "  <dd>$iso_date</dd>\n";
-      $entry_html .= "  <dd>$message</dd>\n";
-      $entry_html .= "  <dd>\n";
-      $entry_html .= "    <ul class=\"filelist\">\n";
-      $entry_html .= $files_html;
-      $entry_html .= "    </ul>\n";
-      $entry_html .= "  <dd>\n";
-      my @paths = keys %displayedPaths;
-
-      if (scalar @paths >= 1) {
-
-        say "$short_id has keys " . Data::Printer::np(@paths);
-        $dl_html .= $entry_html;
-      }
-      else {
-        say "skipping $short_id";
-      }
-    }
-
-    $dl_html .= "</dl>\n";
-
-    # Read the markdown file
-    my $md_content = path($md_file)->slurp_utf8;
-
-    # Replace the placeholder with the definition list
-    $md_content =~ s/<recent-changes><\/recent-changes>/$dl_html/;
-
-    # Write the updated content back to the file
-    path($md_file)->spew_utf8($md_content);
-
-    return scalar(@filtered_entries);   # Return the number of entries processed
-  }
-
+  
   # Generate git history JSON file
   method generate_git_history {
     my ($output_file, $repo_path) = @_;
